eb45e086c1be1866a7d1ebbdd0afdaa498bb8736
Erik Broes <erikbroes@grum.nl>
2010-12-28 07:39:33 +0100

    Added code reformat after applying version specific patches

--- bin/decompile.sh
+++ bin/decompile.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -e
+
 SCRIPT=$(readlink -f $0)
 BASEDIR=`dirname $SCRIPT`/..
 
 JADRETRO=$BASEDIR/tools/jadretro.jar
 # No linux version for this :(
 JAD="wine $BASEDIR/tools/jad.exe"
+
+JACOBE=$BASEDIR/tools/jacobe
+JACOBECFG=$BASEDIR/tools/jacobe.cfg
 
 SERVER=$BASEDIR/minecraft_server.jar
 
@@ -21,6 +26,9 @@ if [[ ! -e $SERVER ]]; then
     exit 0;
 fi
 
+# Dynamicly find the server version
+PATCH=$BASEDIR/patch/`unzip -p $SERVER net/minecraft/server/MinecraftServer.class | strings | grep 'server version' | sed -e 's/^[^0-9]*//' -e 's/_.*//'`.patch
+
 OUTPUT=$BASEDIR/minecraft_server.src.zip
 OUTPUT_TMP=$$.tmp
 OUTPUT_TMP2=$$.tmp2
@@ -30,12 +38,18 @@ mkdir $OUTPUT_TMP
 unzip -d $OUTPUT_TMP $SERVER > /dev/null 2>&1
 
 echo "Preparing classfiles with jadretro"
-java -jar $JADRETRO $OUTPUT_TMP/*{,/*{,/*{,/*}}}/ > /dev/null 2>&1
+java -jar $JADRETRO `find $OUTPUT_TMP -name '*.class' -print` > /dev/null 2>&1
 
 echo "Decompiling with jad"
 mkdir $OUTPUT_TMP2
 $JAD -ff -nonlb -dead -o -r -s .java -d $OUTPUT_TMP2 `find $OUTPUT_TMP -name '*.class' -print` > /dev/null 2>&1
 rm -rf $OUTPUT_TMP
+
+echo "Applying patch to fix some decompilation issues"
+patch -d $OUTPUT_TMP2 -p1 < $PATCH
+
+echo "Reformatting source";
+$JACOBE -cfg=$JACOBECFG -nobackup -overwrite -outext=java $OUTPUT_TMP2/net/minecraft/server/*.java >/dev/null 2>&1
 
 echo "Creating source zip"
 pushd $OUTPUT_TMP2 > /dev/null
